9248bd54b12fc968168b3463c0eee8ceabbf81e4
Sam Win-Mason <sam@albedosoftware.com>
2016-03-29 15:34:28 +1300

    Started work on the django models

--- merlin_api/models.py
+++ merlin_api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Simulation(models.Model):
+    name = models.CharField(max_length=30)
+    num_steps = models.IntegerField(default=1)
+
+
+class UnitType(models.Model):
+    sim = models.ForeignKey(Simulation, on_delete=models.CASCADE)
+    value = models.CharField(max_length=30, unique=True)
+
+
+class Attribute(models.Model):
+    sim = models.ForeignKey(Simulation, on_delete=models.CASCADE)
+    value = models.CharField(max_length=30, unique=True)
