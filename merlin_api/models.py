d5f08d5e2a9b141da8b970bf49e0fa662e8d4342
Sam Win-Mason <sam@albedosoftware.com>
2016-03-29 16:54:17 +1300

    More Django model work. Have defined unit_type, simulation, attribute, ouput and entity models

--- merlin_api/models.py
+++ merlin_api/models.py
@@ -1,15 +1,44 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-class Simulation(models.Model):
+
+class SimObject(models.Model):
+
+    class Meta:
+        abstract = True
+
     name = models.CharField(max_length=30)
-    num_steps = models.IntegerField(default=1)
+
+
+class Simulation(SimObject):
+    num_steps = models.PositiveIntegerField(default=1)
 
 
 class UnitType(models.Model):
+
+    class Meta:
+        unique_together = ('sim', 'value')
+
     sim = models.ForeignKey(Simulation, on_delete=models.CASCADE)
-    value = models.CharField(max_length=30, unique=True)
+    value = models.CharField(max_length=30)
 
 
 class Attribute(models.Model):
+
+    class Meta:
+        unique_together = ('sim', 'value')
+
     sim = models.ForeignKey(Simulation, on_delete=models.CASCADE)
-    value = models.CharField(max_length=30, unique=True)
+    value = models.CharField(max_length=30)
+
+
+class Output(SimObject):
+    sim = models.ForeignKey(Simulation, on_delete=models.CASCADE)
+    unit_type = models.ForeignKey(UnitType, on_delete=models.PROTECT)
+
+
+class Entity(SimObject):
+    sim = models.ForeignKey(Simulation, on_delete=models.CASCADE)
+    attributes = ArrayField(
+        models.ForeignKey(Attribute, on_delete=models.PROTECT))
+    parent = models.ForeignKey('self', on_delete=models.CASCADE)
